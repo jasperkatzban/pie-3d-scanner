e79dfd041b24ba6053b9294391d7eb1273e99688
Jasper Katzban <jasperkatzban@gmail.com>
2021-09-30 19:54:54 -0400

    refactor scanning & increase resolution

--- src/scanner/scanner.ino
+++ src/scanner/scanner.ino
@@ -1,14 +1,14 @@
 #include <Servo.h>
 
 // define measurement resolution
-#define RESOLUTION 5
-#define NUM_POINTS_THETA 10 
-#define NUM_POINTS_PHI 10
-#define THETA_ANGLE_OFFSET 25
-#define PHI_ANGLE_OFFSET 55
+#define RESOLUTION 0.5
+#define NUM_POINTS_THETA 80 
+#define NUM_POINTS_PHI 80
+#define THETA_ANGLE_OFFSET 35
+#define PHI_ANGLE_OFFSET 45
 #define THETA_CENTER_ANGLE 90
 #define PHI_CENTER_ANGLE 90
-#define MOVEMENT_DELAY_MS 500
+#define MOVEMENT_DELAY_MS 100
 
 // create servo object to control a servo
 Servo servo_theta;
@@ -28,6 +28,8 @@ int data[NUM_POINTS_THETA][NUM_POINTS_PHI] = {};
 const uint8_t DEBOUNCE_INTERVAL = 10;
 uint32_t debounce_time;
 bool button_went_back_low;
+
+bool oneservo = false; 
 
 // set up serial comms and servos
 void setup() {
@@ -60,6 +62,28 @@ void loop() {
 // scan across field using servos and sends sensor readings via serial
 void scan(){
   Serial.println("start");
+  if (oneservo){
+    one_servo_scan(); 
+  } else {
+    two_servo_scan(); 
+  }
+  
+  Serial.println("done");
+  reset_servos();
+}
+
+void one_servo_scan(){
+  for (int phi = 0; phi <= NUM_POINTS_PHI; phi++) { // left and right
+    int angle_phi = (phi * RESOLUTION) + PHI_ANGLE_OFFSET;
+    servo_phi.write(angle_phi);
+    servo_theta.write(THETA_CENTER_ANGLE); 
+    float averaged_reading = average_readings();
+    delay(MOVEMENT_DELAY_MS); // wait for the servo to reach the position
+    send_reading(THETA_CENTER_ANGLE, angle_phi, averaged_reading);
+  }
+}
+
+void two_servo_scan(){
   for (int phi = 0; phi <= NUM_POINTS_PHI; phi++) { // left and right
     int angle_phi = (phi * RESOLUTION) + PHI_ANGLE_OFFSET;
     for (int theta = 0; theta <= NUM_POINTS_THETA; theta++) { // up and down
@@ -70,14 +94,14 @@ void scan(){
       }
       servo_phi.write(angle_phi);
       servo_theta.write(angle_theta); 
+      float averaged_reading = average_readings();
       delay(MOVEMENT_DELAY_MS); // wait for the servo to reach the position
-      int sensor_val = analogRead(SENSOR_PIN); 
-      send_reading(angle_theta, angle_phi, sensor_val);
+
+      send_reading(angle_theta, angle_phi, averaged_reading);
     }
   }
-  Serial.println("done");
-  reset_servos();
 }
+
 
 // send position of servos and sensor reading via serial
 void send_reading(int theta, int phi, int sensordata){
@@ -88,6 +112,17 @@ void send_reading(int theta, int phi, int sensordata){
   Serial.println(sensordata);
   Serial.flush();
 }
+
+float average_readings(){
+  int s = 0; 
+  int numpoints = 10; 
+  for(int i = 0; i < numpoints; i++){
+    delay(1);
+    s += analogRead(SENSOR_PIN); 
+  }
+  return s/numpoints; 
+}
+  
 
 // reset servos to center position
 void reset_servos() {
